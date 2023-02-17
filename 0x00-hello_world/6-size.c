ec9a1330d287de77a0f8ef767da00d635b779016
Adel-Essam <adel.essam233@gmail.com>
2023-02-16 17:42:43 -0800

    Hello_world_alx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	pritf("Size of a char : %d byte(s)", sizeof(char));
+	 pritf("Size of a int : %d byte(s)", sizeof(int));
+	  pritf("Size of a long int : %d byte(s)", sizeof(long int));
+	   pritf("Size of a long long : %d byte(s)", sizeof(long long));
+	    pritf("Size of a float : %d byte(s)", sizeof(float));
+	return (0);
+}
