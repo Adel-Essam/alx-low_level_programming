fa75e9dde8a30c62c70440b02222d7efed713c75
Adel-Essam <adel.essam233@gmail.com>
2023-02-17 19:50:38 -0800

    condition_and_loops

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 49 ; i <= 58 ; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+		putchar(' ');
+		putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
