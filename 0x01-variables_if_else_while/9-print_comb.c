5f7f30f6de416e20c3005a775571ea747f1c795a
Adel-Essam <adel.essam233@gmail.com>
2023-02-17 20:01:15 -0800

    condition_and_loops

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry
- * Return: always 0
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48 ; i <= 57 ; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i != 57)
 		{
-		putchar(' ');
-		putchar(',');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
 	return (0);
+}
