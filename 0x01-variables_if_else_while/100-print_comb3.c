4b374ca8eef26fa18f6f5642020fea9e8e1ae022
Adel-Essam <adel.essam233@gmail.com>
2023-02-21 04:45:55 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print_comb3.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(' ');
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
