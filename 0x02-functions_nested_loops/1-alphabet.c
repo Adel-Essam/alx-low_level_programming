065b6ab21988b22565840f0f5e9b4358af55ac5d
Adel-Essam <adel.essam233@gmail.com>
2023-02-21 17:23:08 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i ;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		putchar(i);
+	}
+putchar('\n');
+return (0);
+}
