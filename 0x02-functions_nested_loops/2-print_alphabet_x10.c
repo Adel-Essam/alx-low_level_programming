cf5239d10bfb9f5e4dee8b91d66f6b2b65a9382b
Adel-Essam <adel.essam233@gmail.com>
2023-02-21 17:47:55 -0800

    no.2_print_10_times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i, o;
+
+	for (o = 0 ; o <= 10 ; o++)
+	{
+		for (i = 97 ; i < 123 ; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+return (0);
+}
