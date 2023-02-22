32b644427c638e632d91625967e3c344ae4451ea
Adel-Essam <adel.essam233@gmail.com>
2023-02-22 15:55:46 -0800

    addtion_function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - returns the addtion of 2 numbers.
+ *
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
