e90a74dd0744befe9e75532449ef52108dca556b
Adel-Essam <adel.essam233@gmail.com>
2023-02-22 18:51:58 -0800

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print the numbers that's can be divided by 3 or 5.
+ * *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	int i, sum;
+
+	for (i = 1024 ; i >= 0 ; i--)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+printf("%d", sum);
+return (0);
+}
