047facaf5eae9408771e7e7ea9ed2607ea7889f3
Adel-Essam <adel.essam233@gmail.com>
2023-02-23 16:49:06 -0800

    2-largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-postitive_or_negative();
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c)
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
 
 #endif
