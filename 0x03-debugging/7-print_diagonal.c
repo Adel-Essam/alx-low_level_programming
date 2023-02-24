3804853338ac24456bde7b182093dc595dd85050
Adel-Essam <adel.essam233@gmail.com>
2023-02-23 17:28:27 -0800

    print_diagonal

---
+++ 0x03-debugging/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \ as many as user wants  .
+ * @n: the input no. times
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+int i, j;
+
+for (i = 1 ; i <= n ; i++)
+{
+for (j = 0 ; j <= i ; j++)
+{
+putchar(' ');
+}
+putchar('\\');
+putchar('\n');
+}
+}
