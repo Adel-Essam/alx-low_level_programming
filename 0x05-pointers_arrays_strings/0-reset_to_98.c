82c37f086dae8d53b54fb827e59174496d09d92b
Adel-Essam <adel.essam233@gmail.com>
2023-02-28 16:25:12 -0800

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function changes the value of n .
+ * @n: the input variable
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
