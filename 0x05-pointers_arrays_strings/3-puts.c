d9bc19f404676611e0d651f5e2147cc3f37364d0
Adel-Essam <adel.essam233@gmail.com>
2023-02-28 17:28:54 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - the function prints the same string.
+ * @str: the input variable
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+int i;
+
+i = 0;
+while (i < strlen(str))
+{
+	_putchar(str[i]);
+	i++;
+}
+_putchar('\n');
+}
