e4b1e84c6d09aa487e49c34f9340dce15961b7a7
Adel-Essam <adel.essam233@gmail.com>
2023-03-04 14:46:52 -0800

    6-puts2 added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - the function prints and skpips one character.
+ * @str: the input string
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+	while (sts[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+i++;
+	}
+}
