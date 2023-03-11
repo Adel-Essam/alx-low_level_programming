91864cb20ad7e7a43055f4b81ecfa6afad818bca
Adel-Essam <adel.essam233@gmail.com>
2023-03-10 17:22:23 -0800

    adding 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - the program prints the name of the program.
+ * @argc: the int paramiter
+ * @argv: the string paramiter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
+
