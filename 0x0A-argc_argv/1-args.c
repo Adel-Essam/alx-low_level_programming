2d4d2cd335b883f37e1aba215abfa71f5b92881a
Adel-Essam <adel.essam233@gmail.com>
2023-03-10 17:50:11 -0800

    adding 1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the program prints the number of aguments followed by a new line.
+ * @argc: the int paramiter(number of arguments)
+ * @argv: the string paramiter(the name and arguments after)
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
